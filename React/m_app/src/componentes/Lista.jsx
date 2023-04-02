0f13f92499b236754bf36ef93a6d85e8de9a1d81
Hector Castillo <hectorcastillo280303@gmail.com>
2023-04-02 11:29:21 -0400

    e

---
+++ React/m_app/src/componentes/Lista.jsx
@@ -0,0 +1,20 @@
+const Lista = () => {
+    return ( 
+    <div>
+    <ul>
+        <li>Html</li>
+        <li>Js</li>
+        <li>Css</li>
+        <li>Sass</li>
+    </ul> 
+    <ul>
+        <li>Venezuela</li>
+        <li>Colombia</li>
+        <li>Chile</li>
+        <li>Argentina</li>
+    </ul>
+    </div>
+    );
+}
+ 
+export default Lista;
---
+++ React/m_app/src/vistas/Componente2.jsx
@@ -0,0 +1,10 @@
+const Componente2 = () => {
+    return ( 
+        <>
+        <h1>Componente 2 </h1>
+        <p>Hola este es el comonente 2</p>             
+        </>
+    );
+}
+ 
+export default Componente2;
